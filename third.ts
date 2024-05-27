2bb1f69b660c7acca7dc32aec8b1354dcec7bd92
AYFG <sek82468246@gmail.com>
2024-05-27 17:41:16 +0900

    Requird,Readonly,Record,NonNullable

--- third.ts
+++ third.ts
@@ -32,7 +32,7 @@ const woong3: Partial<Profile> = {
   age: 29,
 };
 
-// Partial 타입 만들어보기
+// Partial 타입 만들어보기 (interface에 적용)
 type Partial_Custom<T> = {
   // interface의 Key들을 꺼내서 ?로 만들어준다
   [Key in keyof T]?: T[Key];
@@ -49,7 +49,7 @@ const woong4: Partial_Custom<Profile> = {
 // Partial의 단점 : 모든 타입을 ?로 만들어서 아예 사용하지 않아도 오류가 안난다.
 const emptyWoong: Partial<Profile> = {};
 
-// utility Types의 Omit(생략), Pick
+// utility Types의 Omit(생략), Pick (interface에 적용)
 const woong5: Omit<Profile, "married"> = {
   // 생략할 키를 적어준다.
   name: "woong",
@@ -66,7 +66,7 @@ const woong6: Pick<Profile, "name" | "age"> = {
 type Pick_Custom<T, S extends keyof T> = {
   [Key in S]: T[Key];
 };
-// Pick 원본
+// Pick 원본 (lib.es5.d.ts)
 // type Pick<T, K extends keyof T> = {
 //   [P in K]: T[P];
 // };
@@ -82,10 +82,10 @@ const woong7: Omit_Custom<Profile, "married"> = {
   name: "woong",
   age: 28,
 };
-// Omit 원본
+// Omit 원본 (lib.es5.d.ts)
 // type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
 
-// Exclude,Extract
+// Exclude,Extract (Key에 적용)
 
 // type Exclude<T, U> = T extends U ? never : T;
 // T에 U 타입이 있으면 never(없애라)로 만들어라
@@ -93,3 +93,70 @@ type ExcludeMarried = Exclude<keyof Profile, "married">;
 // type Extract<T, U> = T extends U ? T : never;
 type ExtractNameAndAge = Extract<keyof Profile, "name" | "age">;
 // T에 U 타입이 있으면 남겨라 U타입이 아니면 never(없애라)로 만들어라
+
+// required
+interface OptionalProfile {
+  name?: string;
+  age?: number;
+  married?: boolean;
+}
+const woong8: Required<OptionalProfile> = {
+  // optional로 만들어진 타입을 필수로 만든다
+  name: "woong",
+  age: 28,
+  married: false,
+};
+// 어떻게 필수로 만들었나?
+type Required_Custom<T> = {
+  [Key in keyof T]-?: T[Key];
+  //   -?  => 옵셔널을 모두 제거해라
+  //   +? => 옵셔널을 추가해라. 그렇지만 ?:와 똑같아서 사용되지 않는다
+};
+// Required 원본 (lib.es5.d.ts)
+// type Required<T> = {
+//     [P in keyof T]-?: T[P];
+// };
+const woong9: Required_Custom<OptionalProfile> = {
+  name: "woong",
+  age: 28,
+  married: false,
+};
+
+// readonly도 제거가 가능하다.
+interface ReadOnlyProfile {
+  readonly name?: string;
+  readonly age?: number;
+  readonly married?: boolean;
+}
+type RemoveReadOnly_Custom<T> = {
+  -readonly [Key in keyof T]-?: T[Key];
+};
+const removeReadOnlyWoong: RemoveReadOnly_Custom<ReadOnlyProfile> = {
+  name: "woong",
+  age: 28,
+  married: false,
+};
+
+// Record : 객체를 표현하는 한 가지 방법
+interface Obj {
+  [key: string]: number;
+}
+const recordObj: Record<string, number> = { a: 3, b: 5, c: 7 };
+// Record 만들기
+type Record_Custom<T extends keyof any, S> = {
+  // type Record_Custom<T, S> = {
+  // 'T' 형식은 'string | number | symbol' 형식에 할당할 수 없습니다.ts(2322)
+  // 객체의 키는 string or number of symbol만 올 수 있기 때문에 T 형식에 extends keyof any를 붙여준다.
+
+  [key in T]: S;
+};
+const recordObj_custom: Record_Custom<string, number> = { a: 3, b: 5, c: 7 };
+
+// non-nullable (Key에 적용)
+type mixedValue = string | null | undefined | boolean | number;
+type primitiveValue = NonNullable<mixedValue>;
+// non-nullable 만들기
+type NonNullable_Custom<T> = T extends null | undefined ? never : T;
+type primitiveValue_Custom = NonNullable_Custom<mixedValue>;
+// non-nullable 원본
+// type NonNullable<T> = T & {};
